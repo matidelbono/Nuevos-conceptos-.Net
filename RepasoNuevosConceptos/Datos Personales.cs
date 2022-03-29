afd90b69c904a3e09d0bd24ea55964b7a1ba8db7
Mati <mati.delbono00@gmail.com>
2022-03-29 11:50:42 -0300

    Primer commit

---
+++ RepasoNuevosConceptos/Datos Personales.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoNuevosConceptos
+{
+    class Datos_Personales
+    {
+        public DateTime FechadeNacimiento { get; set; }
+        public int Edad { get; private set; }
+        public Datos_Personales()
+        {
+        }
+        ~Datos_Personales()
+        {
+            Console.WriteLine("Se destruirà al objeto Datos Personales");
+        }
+
+
+    public void ReemplazarEdad(int nuevaEdad)
+        {
+            this.Edad = nuevaEdad;
+        }
+    }
+  
---
+++ RepasoNuevosConceptos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoNuevosConceptos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo Valores null
+            Nullable<int> EnteroNulable = null;
+            Nullable<double> PuedeQueValgaNull = 9.5;
+            // Propiedades y metodos
+            //Propiedades
+            EnteroNulable = null;
+            int ValorPorDefault = EnteroNulable.GetValueOrDefault();
+            int ValorNoPorDefault = EnteroNulable.GetValueOrDefault(10);
+            EnteroNulable = 100;
+            int ValorReal = EnteroNulable.GetValueOrDefault();
+            //Metodos:
+            //Obtener valor
+            EnteroNulable = 5;
+            int ObtenerValor = EnteroNulable.Value;
+            // Comprobar si tiene valor
+            if (EnteroNulable.HasValue)
+            {
+                Console.WriteLine("tiene valor");
+            }
+            else
+            {
+                Console.WriteLine("no tiene valor");
+            }
+            // Ejemplo para getters y setters
+            Persona persona = new Persona();
+            persona.NombreApellido = "Matias Delbono";
+            Console.WriteLine(persona.NombreApellido);
+            Console.ReadLine();
+            Datos_Personales datos = new Datos_Personales();
+            datos.ReemplazarEdad(10);
+            // Excepciones
+            Console.WriteLine("ingresar un numero positivo");
+            string ingreso = Console.ReadLine();
+
+            try
+            {
+                  NumeroPositivo = int.Parse(ingreso);
+            }
+            catch(Exception e)
+            {
+                NumeroPositivo = -1;
+            }
+    }
+}
