bfba9301e621846aba260a5f5f9b0277718d0ca3
Mati <mati.delbono00@gmail.com>
2022-03-29 11:52:06 -0300

    commit 1

---
+++ RepasoNuevosConceptos/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoNuevosConceptos
+{
+    class Persona
+    {
+        private string nombre { get; set; }
+        private string Apellido { get; set; }
+        public String NombreApellido
+        {
+            get
+          {
+                return $"{nombre},{Apellido}";
+            }
+            set
+            {
+                this.nombre = value.Split('')[0];
+                this.Apellido = value.Split('')[1];
+            }
+}
+    }
+}
---
+++ logica/getters y setters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica
+{
+    public class Class1
+    {
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get {
+                return DateTime.Today.AddTicks(-FechaNacimiento.Ticks).Year - 1;
+        }
+}
+    }
+}
